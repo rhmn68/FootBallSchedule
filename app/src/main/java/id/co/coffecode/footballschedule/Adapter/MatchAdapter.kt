9856bd5bc57084ebe8d365b545e0e3b4cd658974
rhmn69 <auliar68@gmail.com>
2018-10-02 18:21:07 +0700

    Add Match Detail, add Event Detail

--- app/src/main/java/id/co/coffecode/footballschedule/Adapter/MatchAdapter.kt
+++ app/src/main/java/id/co/coffecode/footballschedule/Adapter/MatchAdapter.kt
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.squareup.picasso.Picasso
 import id.co.coffecode.footballschedule.Model.EventsItem
 import id.co.coffecode.footballschedule.R
 import kotlinx.android.synthetic.main.item_match.view.*
--- app/src/main/java/id/co/coffecode/footballschedule/Api/GetApi.kt
+++ app/src/main/java/id/co/coffecode/footballschedule/Api/GetApi.kt
@@ -11,10 +11,6 @@ object GetApi{
         return BuildConfig.BASE_URL + "api/v1/json/${BuildConfig.TSDB_API_KEY}" + "/eventsnextleague.php?id=" + id
     }
 
-    fun getEventDetail(id: String?) : String{
-        return BuildConfig.BASE_URL + "api/v1/json/${BuildConfig.TSDB_API_KEY}" + "/lookupevent.php?id=" + id
-    }
-
     fun getTeamDetail(id: String?) : String{
         return BuildConfig.BASE_URL + "api/v1/json/${BuildConfig.TSDB_API_KEY}" + "/lookupteam.php?id=" + id
     }
